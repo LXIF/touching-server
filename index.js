cbc42d03234d5eeab77eec884a3a0fb9608bf1c9
Andri Schatz <andri.schatz@gmail.com>
2021-04-13 23:49:54 +0200

    uuh

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 //jshint: esversion 6
 
+const serveStatic = require('serve-static');
 const express = require('express');
 const app = express();
 app.use(express.static("public"));
 
-app.get((req, res) => {
-    res.sendFile("index.html")
+// app.get('', (req, res) => {
+//     res.sendFile("index.html")
+// });
+
+var history = require('connect-history-api-fallback');
+
+app.use(history());
+app.use(serveStatic(__dirname + '/dist'));
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+io.on('connection', (socket) => {
+    socket.on('howdy', (message) => {
+        console.log(message);
+    });
+    socket.on('presence-position', (position) => {
+        console.log(position);
+        io.emit('presence-position', position);
+    });
+});
+
+http.listen(process.env.PORT || 3000, () => {
     console.log('listnin yeet');
 });
