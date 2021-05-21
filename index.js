181b6f922509822ec61face4c40e4ba451ece416
LXIF <andri.schatz@gmail.com>
2021-05-21 14:23:31 +0200

    add individual color and poem routings

--- index.js
+++ index.js
@@ -25,10 +25,16 @@ const io = require('socket.io')(http, {
 let users = [];
 let andris = [];
 let rafals = [];
+let usersSockets = [];
+let andrisSockets = [];
+let rafalsSockets = [];
+let touchizationOngoing = false;
+let touchizationMuted = false;
+let nextToucher;
 
 io.on('connection', (socket) => {
 
-    console.log('new connection:' + socket.id);
+
 
     function removeAndri(socket) {
         const andriIndex = andris.findIndex((andri) => {
@@ -36,8 +42,12 @@ io.on('connection', (socket) => {
         });
         if(andriIndex !== -1) {
             andris.splice(andriIndex, 1);
-            // console.log('andri ' + socket.id + ' disconnected');
-            // console.log('andris: ', andris);
+        }
+        const andrisSocketsIndex = andrisSockets.findIndex((andriSocket) => {
+            return andriSocket.id === socket.id;
+        });
+        if(andrisSocketsIndex !== -1) {
+            andrisSockets.splice(andrisSocketsIndex, 1);
         }
     }
 
@@ -46,9 +56,13 @@ io.on('connection', (socket) => {
             return rafal.socket === socket.id;
         });
         if(rafalIndex !== -1) {
-            // console.log('rafal ' + socket.id + ' disconnected');
-            // console.log('rafals: ', rafals);
             rafals.splice(rafalIndex, 1);
+        }
+        const rafalsSocketsIndex = rafalsSockets.findIndex((rafalSocket) => {
+            return rafalSocket.id === socket.id;
+        });
+        if(rafalsSocketsIndex !== -1) {
+            rafalsSockets.splice(rafalsSocketsIndex, 1);
         }
     }
 
@@ -58,9 +72,13 @@ io.on('connection', (socket) => {
         });
         
         if(userIndex !== -1) {
-            // console.log('user ' + socket.id + ' disconnected');
-            // console.log('user: ', user);
             users.splice(userIndex, 1);
+        }
+        const usersSocketsIndex = usersSockets.findIndex((userSocket) => {
+            return userSocket.id === socket.id;
+        });
+        if(usersSocketsIndex !== -1) {
+            usersSockets.splice(usersSocketsIndex, 1);
         }
     }
 
@@ -75,8 +93,6 @@ io.on('connection', (socket) => {
     //handshakeish
 
     socket.on('howdy', (message) => {
-        console.log('socket', socket.id);
-        console.log(message);
 
         if(message.id === 'andri') {
             removeAndri(socket);
@@ -84,6 +100,7 @@ io.on('connection', (socket) => {
                 ...message,
                 socket: socket.id
             });
+            andrisSockets.push(socket);
             socket.join('andris');
         } else if(message.id === 'rafal') {
             removeRafal(socket);
@@ -91,60 +108,260 @@ io.on('connection', (socket) => {
                 ...message,
                 socket: socket.id
             });
+            rafalsSockets.push(socket)
             socket.join('rafals');
-            // console.log(rafals);
+
         } else {
+            console.log(message);
             removeUser(socket);
             users.push({
                 ...message,
                 socket: socket.id
             });
+            usersSockets.push(socket);
             socket.join('users');
-            // console.log(user);
+   
         }
         updateClientsAtAndri()
-        console.log(users);
+        // console.log('connected users: ');
+        // console.log(users);
+        // console.log('connected rafals: ');
+        // console.log(rafals);
+        // console.log('connected andris: ');
+        // console.log(andris);
     });
 
     //local presence
 
     socket.on('presence', (presence) => {
-        // console.log(presence);
+
         socket.broadcast.emit('presence', presence);
     });
 
     //global weather
 
     socket.on('weather', (weather) => {
-        // console.log(weather);
+
         socket.broadcast.emit('weather', weather);
     });
 
     socket.on('chat', (message) => {
-        console.log(message);
+
+    });
+
+    /////////UPDATE USER////////
+
+    socket.on('updateUser', (payload) => {
+        if(payload.id === 'user') {
+            const userIndex = users.findIndex((user) => {
+                return user.socket === socket.id;
+            });
+            if(userIndex !== -1) {
+
+                users[userIndex].audioStarted = true;
+            }
+        }
+        if(payload.id === 'rafal') {
+            const rafalIndex = rafals.findIndex((rafal) => {
+                return rafal.socket === socket.id;
+            });
+            if(rafalIndex !== -1) {
+
+                rafals[rafalIndex].audioStarted = true;
+            }
+        }
+        updateClientsAtAndri();
+    });
+
+    /////////////////////////TOUCHIZATION SEQUENCE//////////////////////
+
+    function startTouchizationSequence() {
+        touchizationOngoing = true;
+        rafals.forEach((rafal) => {
+            rafal.touching = true;
+            socket.join('touching');
+        });
+        io.to('rafals').emit('touchization', 'nowtouched');
+        updateClientsAtAndri();
+        nextToucher = users.find(user => !user.touching);
+        if(!!nextToucher) {
+            io.to(nextToucher.socket).emit('touchization', 'pleasetouch');
+        }
+    }
+
+    function getRandomColor() {
+        return Math.random() * 360;
+    }
+
+    let rafalColorTimer;
+
+    function rafalRandomColors(on) {
+        if(on === true) {
+            rafalColorTimer = setInterval(() => {
+                const newColor = getRandomColor();
+                console.log(newColor);
+                io.to('rafals').emit('touchcolor', newColor);
+            }, 300);
+        } else {
+            console.log('stop');
+            clearInterval(rafalColorTimer);
+        }
+    }
+
+    let usersColorTimer;
+    function usersRandomColors(on) {
+        if(on === true) {
+            usersColorTimer = setInterval(() => {
+                const newColor = getRandomColor();
+                console.log(newColor);
+                io.to('users').emit('touchcolor', newColor);
+            }, 300);
+        } else {
+            console.log('stop');
+            clearInterval(usersColorTimer);
+        }
+    }
+
+    function userIsTouching() {
+        console.log('userIsTouching');
+        const userIndex = users.findIndex((user) => user.socket === socket.id);
+        console.log(userIndex);
+        users[userIndex].touching = true;
+        console.log(users);
+        socket.join('touching');
+        updateClientsAtAndri();
+        nextToucher = users.find(user => !user.touching);
+        const newColor = getRandomColor();
+        io.emit('touchcolor', newColor);
+        if(!!nextToucher) {
+            io.to(nextToucher.socket).emit('touchization', 'pleasetouch');
+        } else {
+            console.log('ausgetoucht');
+            touchizationOngoing = false;
+            touchizationMuted = false;
+        }
+    }
+
+    function skipUserTouching() {
+        console.log('Skip');
+
+        //set touching to true in users
+        const userIndex = users.findIndex((user) => user.socket === nextToucher.socket);
+        console.log(userIndex);
+        users[userIndex].touching = true;
+
+
+        console.log(users);
+
+        //join the socket to the room
+
+        const userSocketIndex = usersSockets.findIndex((userSocket) => userSocket.id === nextToucher.socket);
+        usersSockets[userSocketIndex].join('touching');
+
+        //tell user its been skipped
+        io.to(nextToucher.socket).emit('touchization', 'skip');
+
+        updateClientsAtAndri();
+        nextToucher = users.find(user => !user.touching);
+        const newColor = getRandomColor();
+        io.emit('touchcolor', newColor);
+        if(!!nextToucher) {
+            io.to(nextToucher.socket).emit('touchization', 'pleasetouch');
+        } else {
+            console.log('ausgetoucht');
+            touchizationOngoing = false;
+            touchizationMuted = false;
+        }
+    }
+
+    function resetTouchization() {
+        touchizationOngoing = false;
+        touchizationMuted = false;
+        io.emit('touchization', 'reset');
+    }
+
+    socket.on('touchization', (command) => {
+        console.log(command);
+        if(command === 'rafalColorsOn') {
+            rafalRandomColors(true);
+            io.emit('touchization', 'rafalColorsOn');
+        }
+        if(command === 'rafalColorsOff') {
+            rafalRandomColors(false);
+            io.emit('touchization', 'rafalColorsOff')
+        }
+        if(command === 'usersColorsOn') {
+            usersRandomColors(true);
+            io.emit('touchization', 'usersColorsOn')
+        }
+        if(command === 'usersColorsOff') {
+            usersRandomColors(false);
+            io.emit('touchization', 'usersColorsOff')
+        }
+        // if(command === 'start') {
+        //     //startTouchizationSequence();
+        //     sendColorToAll = !sendColorToAll;
+        //     if(sendColorToAll) {
+        //         io.to('users').emit('touchization','nowtouched');
+        //     } else {
+        //         io.to('users').emit('touchization','nownottouched');
+        //     }
+        // }
+        // if(command === 'yestouch' && touchizationOngoing) {
+        //     userIsTouching(socket.id);
+        // }
+        // if(command === 'muteon') {
+        //     touchizationMuted = true;
+        //     io.emit('touchization', 'muteon');
+        //     rafalRandomColors(false);
+        // }
+        // if(command === 'muteoff') {
+        //     touchizationMuted = false;
+        //     io.emit('touchization', 'muteoff');
+        //     rafalRandomColors(true);
+        // }
+        // if(command === 'skip') {
+        //     skipUserTouching();
+        // }
+        // if(command === 'reset') {
+        //     resetTouchization();
+        // }
+    });
+
+    socket.on('poem', (message) => {
+        if(message.outputusers) {
+            io.to('rafals').emit('poem', message);
+        }
+        if(message.outputUsers) {
+            io.to('users').emit('poem', message);
+        }
     });
 
 
-    socket.on('KnockKnock', (data) => {
-        console.log('knockknock');
-        socket.emit('WhosThere');
-    });
+    // socket.on('touchsequence', (message) => {
+    //     //start
+    //     if(message === 'start') {
+    //         let counter = 0;
 
-    socket.on('ItsMe', (data) => {
-        socket.emit('Welcome', 'Hi customer using unity' + data.version + ', this is backend microservice ' + process.env.K8S_POD + '. Thanks for buying our asset. (No data is stored on our server)');
-        socket.emit('TechData', {
-			podName: process.env.K8S_POD,
-			timestamp: (new Date()).toUTCString()
-		});
-		socket.disconnect(true);
-    });
+
+    //     }
+    //     //requesttouch
+    //     //sendtouch
+    //     //end
+    //     if(message === 'end') {
+
+    //     }
+    // });
+
+    // socket.on('sendtouch', () => {
+
+    // });
 
 
 
     //disconnect
 
     socket.on('disconnect', () => {
-        console.log('dis');
         removeUser(socket);
         removeAndri(socket);
         removeRafal(socket);
@@ -154,5 +371,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(process.env.PORT || 3000, () => {
-    console.log('listnin yeet');
+    console.log('listnin 3K yeet');
 });
